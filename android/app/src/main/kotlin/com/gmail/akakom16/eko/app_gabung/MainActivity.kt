38a2e311bf1b81f9cf50ca1c10640d407da54ff5
Ekogunawan <48266816+Ekogunawan@users.noreply.github.com>
2019-10-27 22:20:31 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/gmail/akakom16/eko/app_gabung/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.akakom16.eko.app_gabung
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
